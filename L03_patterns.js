a89816c547c5d287f3158ae8b93f73e28448de0e
LuLuCheeky <lulucheekypro@gmail.com>
2025-07-28 10:19:36 +0000

    Jul 28, 2025, 10:19 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -6,14 +6,27 @@ function setup() {
     background(220);
     //noLoop(); // Stops continuous drawing
   }
-  
+
 function draw() {
-  for (let i = 0; 1 < 8; i++){
-    if (i % 2 === 0) {
-      fill (0);
-    } else {
-      fill (255);
+  // for (let i = 0; 1 < 8; i++){
+  //   if (i % 2 === 0) {
+  //     fill (0);
+  //   } else {
+  //     fill (255);
+  //   }
+  //   circle (spacing + i * spacing, 200, 40);
+  // }
+
+  for (let i = 0; i<5; i++) {
+        // fill(i*spacing);
+        //fill(colorValue%255);
+        if(i%2 === 0) {
+            fill(0);
+        } else {
+            fill(255);
+        }
+        circle(spacing + i*spacing, 200, 40); 
+        rect(spacing + i*spacing,400, 40, 40);
+        //colorValue +=1; 
     }
-    circle (spacing + i * spacing, 200, 40);
-  }
 }
