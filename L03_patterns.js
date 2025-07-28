d607d56eb314f238f441f9ef59a2c4d9d8ddfba4
LuLuCheeky <lulucheekypro@gmail.com>
2025-07-28 10:10:52 +0000

    Jul 28, 2025, 10:10 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -7,5 +7,7 @@ function setup() {
     //noLoop(); // Stops continuous drawing
   }
 function draw() {
-  for (let i = 0; 1 < 8; i++)
+  for (let i = 0; 1 < 8; i++){
+    if (i % 2 === 0) {}
+  }
 }
