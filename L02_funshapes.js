c9e08d2bbf7425b2438da974597a0621e0535558
LuLuCheeky <lulucheekypro@gmail.com>
2025-07-28 09:34:44 +0000

    Jul 28, 2025, 9:34 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -1,19 +1,11 @@
-/*
-Task 1: Add the setup() function
-*/
+function setup() {
+  createCanvas(1536,728);
+  background(255,255, 125);
+}
 
 function draw() {
-  // Challenge 1: An eye using ellipse() function
-
-  // Challenge 2: A face using circle() function
-
-  // Challenge 3: A house using rect() function
-
-  // Challenge 4: A rocketship using triangle() function
-
-  // Challenge 5: A square face using quad() function
-
-  // Challenge 6: Add some colours to the drawings in the above challenges
-
-  // Challenge 7: Create a funny Pokemon
+triangle(100, 100, 200, 50, 300, 100);
+quad(400, 100, 500, 50, 600, 100, 550, 200);
+  ellipse(800, 100, 100, 100);
+  rect(900, 50, 200, 200);
 }
--- L03_patterns.js
+++ L03_patterns.js
@@ -5,13 +5,5 @@ function setup() {
   }
   
 function draw() {
-  // Recap 1: Repeating Circles
-
-  // Task 1: Colour Gradient
-
-  // Task 2: Colour Loop
-
-  // Task 3: Row of Circles
-
-  // Task 4: Grid of Circles
+  circle()
 }
