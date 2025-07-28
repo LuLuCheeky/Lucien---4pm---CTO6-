d5b0d7c1fc1d326e1e345e3a61091ce53011571e
LuLuCheeky <lulucheekypro@gmail.com>
2025-07-28 10:32:45 +0000

    Jul 28, 2025, 10:32 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -21,8 +21,8 @@ function draw() {
   //       rect(spacing + i*spacing,400, 40, 40);
   //       //colorValue +=1; 
   // }
-  fill(0);
-  rect(0 , 50, );
+  fill(255);
+  rect(0 , 50, width, height - 100);
   fill(255);
   circle(width / 2, height / 2, 100);
   
