7099c7f1aa0ba156d91997b8167a88648ccf8b54
LuLuCheeky <lulucheekypro@gmail.com>
2025-08-10 09:29:03 +0000

    Aug 10, 2025, 9:29 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -12,5 +12,5 @@ function mouseDragged() {
     my = random(-10, 10)
     shapeColour = color(random(255), random(255), random(255));
     fill(shapeColour);
-    circle(mouseX + mx, mouseY + my, random(1, 50));
+    circle(mouseX + mx, mouseY + my, random(10, 50));
 }
