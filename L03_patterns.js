a88baa1df0945a24ef353f0fc2a4e70407430e8e
LuLuCheeky <lulucheekypro@gmail.com>
2025-08-10 08:19:31 +0000

    Aug 10, 2025, 8:19 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -63,7 +63,7 @@ function draw() {
   let startX = (width - totalWidth) / 2 + circleDiameter / 2;
   let startY = (height - totalWidth) / 2 + circleDiameter / 2;
   for (let j = 0; j < numCircles; j++) {
-    let Y = startY + (j * circleDiameter);
+    let Y = ;
 
     for (let i = 0; i < 5; i++) {
       fill(0, i * 50, 0)
