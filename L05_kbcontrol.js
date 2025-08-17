14601205de229eb3a818bd6298d1e4020b57a12d
LuLuCheeky <lulucheekypro@gmail.com>
2025-08-17 09:29:45 +0000

    Aug 17, 2025, 9:29 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -40,7 +40,7 @@ function keyPressed() {
     }else if (keyCode === 85) {
         shapeColour = "darkMagenta";
     }else if (keyCode === 73) {
-        shapeColour = "dakrkOrchid";
+        shapeColour = "darkOrchid";
     }else if (keyCode === 79) {
     }
     
