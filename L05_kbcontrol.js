3d9ed89bc7ebf3f8f4589702346e526b0230816a
LuLuCheeky <lulucheekypro@gmail.com>
2025-08-17 09:05:03 +0000

    Aug 17, 2025, 9:05 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -20,7 +20,7 @@ function setup(){
 function draw() {
     background(220);
     if (showCircle) {
-        circle(width / 2, height  / 2, 100);
+        circle(width / 2, height  / 2, 20);
     }
 }
 
