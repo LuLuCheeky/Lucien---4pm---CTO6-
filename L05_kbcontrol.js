7b43c0a7721b2c48483ba840fd6af9c4d2b8bbf0
LuLuCheeky <lulucheekypro@gmail.com>
2025-08-17 08:12:27 +0000

    Aug 17, 2025, 8:12 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -14,8 +14,6 @@ function mousePressed(){
 }
 
 function mouseDragged(){
-    let mx = random(-10, 10);
-    let my = random(-10, 10);
     shapeColour = color(random(255), random(255), random(255));
     fill(shapeColour);
     circle(mouseX + mx, mouseY + my, CircleSize);
