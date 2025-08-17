9177a6531527cdedd60dff1f4a826e4771ccb5ec
LuLuCheeky <lulucheekypro@gmail.com>
2025-08-17 09:17:11 +0000

    Aug 17, 2025, 9:17 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,4 +1,4 @@
-let showCircle = false;
+
 
 function setup(){
     createCanvas(400, 400);
@@ -20,7 +20,7 @@ function setup(){
 function draw() {
     background(220);
     circle(width / 2, height / 2, 100);
-    fill()
+    fill(shapeColour);
 }
 
 function keyPressed() {
