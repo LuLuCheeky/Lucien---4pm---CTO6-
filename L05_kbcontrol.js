fb177f0bfcec68ff324df592660ef00afa8156d0
LuLuCheeky <lulucheekypro@gmail.com>
2025-08-17 09:21:10 +0000

    Aug 17, 2025, 9:21 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -25,7 +25,7 @@ function draw() {
 
 function keyPressed() {
     if (keyCode === UP_ARROW) {
-        shapeColour = 'neon ';
+        shapeColour = ';
     }else if (keyCode === DOWN_ARROW) {
         shapeColour = 'black';
     }
