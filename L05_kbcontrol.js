2044b1634ff380a6145e3a8d802ad3881a3ee7e1
LuLuCheeky <lulucheekypro@gmail.com>
2025-09-07 09:25:57 +0000

    Sep 7, 2025, 9:25 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,4 +1,4 @@
-let x =constraint();
+
 let speed = 5;
 function setup(){
     createCanvas(600, 400);
@@ -10,7 +10,7 @@ function setup(){
 function draw(){
     background(220);
 
-
+    let x 
     circle(width / 2, height / 2, 50);
 }
 
