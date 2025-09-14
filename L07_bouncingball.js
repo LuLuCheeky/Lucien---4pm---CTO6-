ee0532a9f8e7eb34c14b1de914c99ef43f2df9fc
LuLuCheeky <lulucheekypro@gmail.com>
2025-09-14 08:22:22 +0000

    Sep 14, 2025, 8:22 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -2,6 +2,6 @@ let soundEffect, bgMusic, staticImage;
 
 function preload(){
     soundEffect = loadSound("assets/pop.mp3");
-    bgMusic = loadSound("assets/bossa");
+    bgMusic = loadSound("assets/bossaNova.mp3");
     staticImage = loadImage("assets/cat.png");
 }
