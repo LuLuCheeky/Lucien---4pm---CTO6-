2b1439ea70806490d6bf367e0b90ccc8cd73fcff
LuLuCheeky <lulucheekypro@gmail.com>
2025-09-14 08:48:19 +0000

    Sep 14, 2025, 8:48 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -20,8 +20,20 @@ function setup(){
 function draw(){
     background(220);
     image(staticImage, x, y, 50, 50);
+
+
+    
     if(keyIsDown(LEFT_ARROW)){
         x -= 5;
+    }
+    if(keyIsDown(RIGHT_ARROW)){
+        x += 5;
+    }
+    if(keyIsDown(UP_ARROW)){
+        y -= 5;
+    }
+    if(keyIsDown(DOWN_ARROW)){
+        y += 5;
     }
 }
 
