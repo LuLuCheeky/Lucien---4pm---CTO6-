84710645b944b7c81238892e827797ccd8f49f7e
LuLuCheeky <lulucheekypro@gmail.com>
2025-09-14 09:34:38 +0000

    Sep 14, 2025, 9:34 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -58,8 +58,8 @@ let shapeColour = 'blue';
 let x = 300;
 let y = 200;
 let size = 100;
-let xSpeed = 1000;
-let ySpeed = 1000;
+let xSpeed = 50;
+let ySpeed = 50;
 
 function setup(){
     createCanvas(600, 400);
