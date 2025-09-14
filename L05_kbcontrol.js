3cd151e005d75ec180ebd0896a2acef97caef21c
LuLuCheeky <lulucheekypro@gmail.com>
2025-09-14 08:17:06 +0000

    Sep 14, 2025, 8:17 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -11,8 +11,8 @@ function setup(){
 function draw(){
     background(220);
 
-    x = constrain(x, 50, width - 50);
-    y = constrain(y, 50, height - 50);
+    x = constrain(x, 25, width - 25);
+    y = constrain(y, 25, height - 25);
 
     if(keyIsDown(68)){
         x += speed;
