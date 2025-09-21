bc53ac628c9718ddde840a5e83c6f441a66afebb
LuLuCheeky <lulucheekypro@gmail.com>
2025-09-21 09:26:42 +0000

    Sep 21, 2025, 9:26 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -9,6 +9,6 @@ function draw() {
     textAlign(LEFT);
     text("My favourite foods are:", 20, 30);
     for (let i = 0; i < 5; i++) {
-        text(favouriteFood[i], 20, 60 + i * 30);
+        text(i + 1,favouriteFood[i], 20, 60 + i * 30);
     }
 }
