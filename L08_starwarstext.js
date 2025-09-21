05572f4699386131f455d558f321d14d90dd4f98
LuLuCheeky <lulucheekypro@gmail.com>
2025-09-21 08:44:33 +0000

    Sep 21, 2025, 8:44 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,5 +1,11 @@
 function setup(){
     createCanvas(600, 400);
-    backgorund(220);
+    background(220);
 }
 
+function draw(){
+    fill(255, 204, 0);
+    textSize(32);
+    text('A long time ago in a galaxy far,', 10, 50);
+    text('far away....', 10, 100);
+}
