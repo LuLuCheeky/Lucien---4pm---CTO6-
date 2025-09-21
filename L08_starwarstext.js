1df1954d84b7df063d89ef20a3b0def60d5710c6
LuLuCheeky <lulucheekypro@gmail.com>
2025-09-21 09:05:14 +0000

    Sep 21, 2025, 9:05 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -2,5 +2,6 @@ function setup() {
     createCanvas(600, 400);
     textAlign(CENTER);
     textSize(32);
-    text("My name is  ")
+    text("My name is Mr Beast", width / 2, height / 2);
+    text("I am 100 this year", width / 2, height / 2 + 40);
 }
