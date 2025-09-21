7b884c88ccbdf779ea904fdfee268c248df2e7e2
LuLuCheeky <lulucheekypro@gmail.com>
2025-09-21 08:50:59 +0000

    Sep 21, 2025, 8:50 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,7 +1,7 @@
 let xSpeed = 5;
 let x = width / 2;
 let y = height / 2;
-let size = 10
+let size = 50;
 function setup(){
     createCanvas(600, 400);
     background(220);
@@ -9,10 +9,13 @@ function setup(){
 
 function draw(){
     background(220);
-    if (x <= 0 || x >= width - size / 2){
+    if (x <= 0 || x >= width - size){
         xSpeed*= -1;
         shapeColour = color(random(255), random(255), random(255));
     }
-    rect(x, y, 50, 50);
+
     x += xSpeed;
+
+    noStroke();
+    rect(x, y, size, size);
 }
