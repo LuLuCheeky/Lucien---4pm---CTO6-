21e30c9c43a3658a4c51304bda46f87dff12e0f7
LuLuCheeky <lulucheekypro@gmail.com>
2025-09-28 08:24:21 +0000

    Sep 28, 2025, 8:24 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -29,8 +29,7 @@ function draw() {
     for (let i = 0; i < storyText.length; i++) {
         text(storyText[i], width / 2, yPos + i * 50);
         yPos -= 0.1;
-        if (yPos < - storyText.length * 24 + (storyText.length - 1) * 50) {
-            yPos = height;
-        }
+    if (yPos < - storyText.length * 24 + (storyText.length - 1) * 50) {
+            yPos = height;}
     }
 }
