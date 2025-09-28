35bdb7922a22f39867c3c1b13a5f1027e2d3e190
LuLuCheeky <lulucheekypro@gmail.com>
2025-09-28 08:57:52 +0000

    Sep 28, 2025, 8:57 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -3,6 +3,18 @@ let x = 300;
 
 function setup() {
   createCanvas(600, 400);
-  textSize(32);
+  textSize(24);
   textAlign(CENTER, CENTER);
+}
+
+function draw() {
+    background(220);
+
+    if(x <= 0 || x >= width - 3.5 * 24) {
+        speed *= -1;
+    }
+
+    x -= speed;
+
+    text("Hi", x, height / 2);
 }
