1fdf105cf4373c30e86289ba386390902bb22fed
LuLuCheeky <lulucheekypro@gmail.com>
2025-09-28 08:44:19 +0000

    Sep 28, 2025, 8:44 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -35,18 +35,20 @@ function mousePressed() {
 }    
 
 function draw() {
-    background(0);
-    textSize(24);
-    textAlign(CENTER);
-    fill("yellow");
-    for (let i = 0; i < storyText.length; i++) {
-        text(storyText[i], width / 2, yPos + i * 50);
-    }
+    if (bgMusic && bgMusic.isPlaying()) {
+        background(0);
+        textSize(24);
+        textAlign(CENTER);
+        fill("yellow");
+        for (let i = 0; i < storyText.length; i++) {
+            text(storyText[i], width / 2, yPos + i * 50);
+        }
     
-    yPos -= 1;
+        yPos -= 1;
 
-    if (yPos < - (storyText.length * 24 + (storyText.length - 1) * 50)) {
-        yPos = height;
+        if (yPos < - (storyText.length * 24 + (storyText.length - 1) * 50)) {
+            yPos = height;
 
+        }
     }
 }
