cf6f30160481eda1cd60b2d7ceb8e4b93feda308
LuLuCheeky <lulucheekypro@gmail.com>
2025-09-28 09:26:23 +0000

    Sep 28, 2025, 9:26 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -50,7 +50,7 @@ function setup() {
 
 function draw() {
     background(220);
-    text(countdownNum, width / 2, height / 2);
+    text(countdownNum, width / 2, height / 2 - 50);
 }
 
 function countdown() {
