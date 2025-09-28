8546bb87f5df4582770d77fee0cab2ec437e5264
LuLuCheeky <lulucheekypro@gmail.com>
2025-09-28 08:06:32 +0000

    Sep 28, 2025, 8:06 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,14 +1,14 @@
-let favouriteFood = ["poop", "snot", "boogers", "earwax", "toe jam"];
-function setup() {
-    createCanvas(600, 400);
-    background(220);
-}
+// let favouriteFood = ["poop", "snot", "boogers", "earwax", "toe jam"];
+// function setup() {
+//     createCanvas(600, 400);
+//     background(220);
+// }
 
-function draw() {
-    textSize(20);
-    textAlign(LEFT);
-    text("My favourite foods are:", 20, 30);
-    for (let i = 0; i < 5; i++) {
-        text(i + 1 + ". " + favouriteFood[i], 20, 60 + i * 30);
-    }
-}
+// function draw() {
+//     textSize(20);
+//     textAlign(LEFT);
+//     text("My favourite foods are:", 20, 30);
+//     for (let i = 0; i < 5; i++) {
+//         text(i + 1 + ". " + favouriteFood[i], 20, 60 + i * 30);
+//     }
+// }
