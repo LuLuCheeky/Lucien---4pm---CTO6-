659f21bf7d52c6f8b66240755f436511d280b568
LuLuCheeky <lulucheekypro@gmail.com>
2025-10-12 09:21:34 +0000

    Oct 12, 2025, 9:21 AM

--- L10_name.js
+++ L10_name.js
@@ -1,25 +1,26 @@
-let userInput;
-let text = "TEXT WOW 😲😲"
-let userText
+// let userInput;
+// let text = "TEXT WOW 😲😲"
+// let userText
 
-function setup() {
-    createCanvas(600,600);
-    background(220);
-    input = createInput();
-    input.position(width / 2 - input.width / 2, height - 50);
-    userInput.input(userInputUp);
+// function setup() {
+//     createCanvas(600,600);
+//     background(220);
+//     input = createInput();
+//     input.position(width / 2 - input.width / 2, height - 50);
+//     userInput.input(userInputUp);
 
-}
+// }
 
-function draw() {       
-    background(220);
+// function draw() {       
+//     background(220);
 
-    textAlign(CENTER, BOTTOM);
-    textSize(32);
-    fill(0);
-    text("Type something :D", 50, height - 50);
-}
+//     textAlign(CENTER, BOTTOM);
+//     textSize(32);
+//     fill(0);
+//     text("Type something :D", 50, height - 50);
+// }
 
-function userInputUp() {
-    user.text = this.value();
-}
+// function userInputUp() {
+//     user.text = this.value();
+// }
+
