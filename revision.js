2bc6080903f7657071a7d4eef2369ba8a2220a97
LuLuCheeky <lulucheekypro@gmail.com>
2025-10-12 08:40:51 +0000

    Oct 12, 2025, 8:40 AM

--- revision.js
+++ revision.js
@@ -1,24 +1,25 @@
-let x = 300;
-let y;
-let speed = 2;
+// let x = 300;
+// let y;
+// let speed = 2;
 
-function setup() {
-    createCanvas(600, 400);
-    background("lightblue");
-    noStroke();
-}
+// function setup() {
+//     createCanvas(600, 400);
+//     background("lightblue");
+//     noStroke();
+// }
 
-function draw() {
-    background("lightblue");
-    y = height / 2;
-    fill(220,0,0);
-    x += speed;
-    circle(x, y, 50);
-    if (x > width - 27) {
-        speed = -2;
-    }
-    if (x < 27) {
-        speed = 2;
-    }
-    x = constrain(x, 25, width - 25);
-}
+// function draw() {
+//     background("lightblue");
+//     y = height / 2;
+//     fill(220,0,0);
+//     x += speed;
+//     circle(x, y, 50);
+//     if (x > width - 27) {
+//         speed = -2;
+//     }
+//     if (x < 27) {
+//         speed = 2;
+//     }
+//     x = constrain(x, 25, width - 25);
+// }
+
