c62b3ed86c219abb79afab7ac112cec97abd948d
LuLuCheeky <lulucheekypro@gmail.com>
2025-10-19 09:17:39 +0000

    Oct 19, 2025, 9:17 AM

--- revision.js
+++ revision.js
@@ -62,13 +62,13 @@ function setup() {
 
 function draw() {
     background(colourPicker.value());
-    textAlign(LEFT)
+    textAlign(LEFT);
     textSize(15);
     text("Enter your name: ", width / 2 - 175, height / 2 + 100);
 
     rect(width / 2 - 125, height / 2 - 100,width / 2 + 50,height / 2 - 50,25 );
     text(userText, width / 2 , height / 2);
-
+    
 
 }
 
