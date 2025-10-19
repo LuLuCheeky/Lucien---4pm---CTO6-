1a02ec034452920c6e83b7d3b424595732b19ab5
LuLuCheeky <lulucheekypro@gmail.com>
2025-10-19 08:05:38 +0000

    Oct 19, 2025, 8:05 AM

--- revision.js
+++ revision.js
@@ -43,7 +43,7 @@ let colourPicker;
 function setup() {
     createCanvas(400, 400);
     
-    colourPicker = createColorPicker('orange');
+    colourPicker = createColorPicker('yellow');
     colourPicker.position(width / 2, height);
 
 }
