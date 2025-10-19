66062f2e9cc014ab3cfaca65c488d15a8665ed44
LuLuCheeky <lulucheekypro@gmail.com>
2025-10-19 09:18:35 +0000

    Oct 19, 2025, 9:18 AM

--- revision.js
+++ revision.js
@@ -68,7 +68,7 @@ function draw() {
 
     rect(width / 2 - 125, height / 2 - 100,width / 2 + 50,height / 2 - 50,25 );
     text(text1, width / 2 , height / 150);
-    text
+    text(text2, width / 2 , height / 200);
 
     
 
