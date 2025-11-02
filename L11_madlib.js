10ee7365748fc3d9eb8d985cc2c24966c3fc9ea6
LuLuCheeky <lulucheekypro@gmail.com>
2025-11-02 08:32:08 +0000

    Nov 2, 2025, 8:32 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -23,6 +23,6 @@ function draw() {
 
     for(let i = 0; i < words.length; i++){
         textSize(20);
-        text(words[i], 50, 100 + i * 40);
+        text(words[i], 50, 100 + i * 45);
     }
 }
