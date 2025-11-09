4dd862d37e221fb74639d54328a840a2225ea649
LuLuCheeky <lulucheekypro@gmail.com>
2025-11-09 08:23:49 +0000

    Nov 9, 2025, 8:23 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -1,2 +1,13 @@
+let attempts = 0;
+
 function setup() {
     createCanvas(600,600);
+}
+
+function draw() {
+    background(220);
+
+    textAlign(CENTER, CENTER);
+    text("Guess the Hidden Word!", width / 2, 50);
+    text("Attempts:" + attempts, width / 2, 100);
+}
