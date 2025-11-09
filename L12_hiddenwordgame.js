3b9e3e832ebbb469d967e4e5a358effbac0baf3c
LuLuCheeky <lulucheekypro@gmail.com>
2025-11-09 08:45:10 +0000

    Nov 9, 2025, 8:45 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -9,7 +9,7 @@ function setup() {
     textBox = createInput();
     textBox.position(width / 2 - textBox.width / 2, 200);
     textBox.size(150, 30);
-    textBox.style()
+    textBox.style('font-size', '16px');
 
     button = createButton("Submit Guess");
     button.position(textBox.x + textBox.width + 30, 200);
